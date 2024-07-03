17759f952a978133320e14f05ddf692c68065706
Stevo <scanners101@gmail.com>
2024-07-03 10:33:36 +0000

    S3 bucket test 6

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "swbswe-s3-bucket"
   acl    = "private"
 
   versioning = {
