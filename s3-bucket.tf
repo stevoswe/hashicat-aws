082d5e5ea5ace88039bba55e37f33f52c5f0cd04
Stevo <scanners101@gmail.com>
2024-07-03 10:23:47 +0000

    S3 bucket test 3

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,9 +2,8 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "Gaurav"
+  bucket = "steven-beazley-Gaurav"
   acl    = "private"
-  bucket_prefix = "steven-beazley"
 
   versioning = {
     enabled = true
